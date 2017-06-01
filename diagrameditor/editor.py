4d0a86801622c8468529eedce9e443612a7c0289
root <spamseanluchen@gmail.com>
2017-06-01 16:12:01 -0400

    finally got embedding for jupyter to work

---
+++ diagrameditor/editor.py
@@ -0,0 +1,9 @@
+import IPython
+
+class Editor:
+    def __init__():
+        url = 'diagrameditor/diagrameditor.html'
+        iframe = '<iframe src=' + url + ' width=780 height=740></iframe>'
+        IPython.display.HTML(iframe)
+    def export():
+
