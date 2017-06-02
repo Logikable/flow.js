cd04567f369240c3fcab76c9553388dc3824c343
Logikable <spamseanluchen@gmail.com>
2017-06-02 14:31:17 -0400

    changed border of page from 20px to 0px

--- diagrameditor/editor.py
+++
@@ -1,9 +0,0 @@
-import IPython
-
-class Editor:
-    def __init__():
-        url = 'diagrameditor/diagrameditor.html'
-        iframe = '<iframe src=' + url + ' width=780 height=740></iframe>'
-        IPython.display.HTML(iframe)
-    def export():
-
